2b7678982bbb3edced2fcfc7d625bd933a3c1a10
Edgar Harutyunyan <edgar.harutiunyan@icloud.com>
2024-02-13 09:14:00 +0400

    Added Basic add API Test and config for RestAssured

---
+++ src/main/java/com/talxan/ywitter/RestAssured/RestAssuredConfig.java
@@ -0,0 +1,71 @@
+package com.talxan.ywitter.RestAssured;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import jdk.jshell.execution.Util;
+
+import java.io.IOException;
+
+import static io.restassured.http.ContentType.JSON;
+
+public class RestAssuredConfig {
+    public static Response registerUser(String s) throws IOException {
+
+        return RestAssured.given()
+                .baseUri("http://localhost:8080/api/v1/auth/register")
+                .contentType(JSON)
+                .accept(JSON)
+                .body(s)
+                .post();
+
+    }
+
+//    /**
+//     * Gets a board given the ID
+//     * @param boards
+//     * @return Response Type
+//     */
+//    public static Response getBoard(){
+//
+//        Response response = RestAssured.given()
+//                .get();
+//        return response;
+//
+//    }
+//
+//    /**
+//     * Updates the board given the board and the new parameter
+//     * @param boards
+//     * @param value new parameter
+//     * @param key key to be changed
+//     * @return Response Type
+//     */
+//    public static Response updateBoard() {
+//
+//        Response response = RestAssured.given()
+//                .queryParam()
+//                .put();
+//        return response;
+//
+//    }
+//
+//
+//    public static Response deleteBoard(){
+//
+//        Response response = RestAssured.given()
+//                .delete();
+//        return response;
+//
+//    }
+
+
+    //Helper method to generate the URL for the needed operations
+    private static RequestSpecification generateURL() {
+
+        return RestAssured.given()
+                .baseUri(Utility.BASE_URI)
+                .basePath(Utility.BASE_PATH);
+
+    }
+}
---
+++ src/main/java/com/talxan/ywitter/RestAssured/Utility.java
@@ -0,0 +1,19 @@
+package com.talxan.ywitter.RestAssured;
+
+import java.util.Optional;
+
+public class Utility {
+    public static final String BASE_URI = "http://localhost:8080";
+    public static final String BASE_PATH = "api/v1";
+
+
+//    public static String getBasePath(String id) {
+//
+//        return Optional
+//                .ofNullable(id)
+//                .map(id1 -> TrelloAPIUtil.BOARDS_PATH + "/" + id1)
+//                .orElse(TrelloAPIUtil.BOARDS_PATH);
+//
+//    }
+
+}
