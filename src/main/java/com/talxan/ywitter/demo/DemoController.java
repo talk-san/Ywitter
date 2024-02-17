f4fd4503ed42f33ee9167c4d21f0c79aae7ab677
Tariel Hakobyan <tariel.hakobyan03@gmail.com>
2024-02-18 00:17:11 +0400

    ADD: CorsFilter to allow Frontend Requests

--- src/main/java/com/talxan/ywitter/demo/DemoController.java
+++ src/main/java/com/talxan/ywitter/demo/DemoController.java
@@ -5,12 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("api/v1/demo-controller")
+@RequestMapping("api/v1/open")
 public class DemoController {
-    @GetMapping
-    public ResponseEntity<String> sayHello() {
-        return ResponseEntity.ok("Hello from secured endpoint");
+    @GetMapping("/messages")
+    public ResponseEntity<List<String>> messages() {
+        return ResponseEntity.ok(List.of("message 1 from backend", "message 2"));
     }
 
 }
