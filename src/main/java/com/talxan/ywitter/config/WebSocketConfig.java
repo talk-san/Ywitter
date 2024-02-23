d349874893e931838fc2663e1d5726f1bb61ea7b
Edgar Harutyunyan <edgar.harutiunyan@icloud.com>
2024-02-23 15:29:02 +0400

    ADD: Configuration for the WebSocket

--- src/main/java/com/talxan/ywitter/config/WebSocketConfig.java
+++ src/main/java/com/talxan/ywitter/config/WebSocketConfig.java
@@ -1,4 +1,44 @@
 package com.talxan.ywitter.config;
 
-public class WebSocketConfig {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSocketMessageBroker
+
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/user");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.setUserDestinationPrefix("/user");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws").withSockJS();
+    }
+
+    @Override
+    public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+        MappingJackson2MessageConverter J2MConverter = new MappingJackson2MessageConverter();
+        J2MConverter.setObjectMapper(new ObjectMapper());
+        J2MConverter.setContentTypeResolver(resolver);
+        messageConverters.add(J2MConverter);
+
+        return false;
+    }
 }
